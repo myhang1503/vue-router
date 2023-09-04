2c33bd53050620464ff1e72010a3c7f249cd495b
ptmhang <myhang1503@gmail.com>
2023-09-04 21:40:01 +0700

    setup router link

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="p-3 mb-2 bg-dark text-white">
+    <ul class="d-flex justify-content-center align-items-center">
+        <li>
+            <router-link class="mx-3" to="/users">Users</router-link>
+        </li>
+        <li>
+            <router-link class="mx-3" to="/pets">Pets</router-link>
+        </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+ul{
+    list-style: none;
+}
+a{
+    display: block;
+    padding: 5px 5px;
+    color: unset;
+}
+a:hover {
+    color: palevioletred;
+    text-decoration: none;
+}
+.active{
+    color: palevioletred;
+    border-bottom: 2px solid palevioletred;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import UserList from "./pages/UserList.vue";
+import PetList from "./pages/PetList.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-createApp(App).mount('#app')
+const routes = [
+  { path: "/users", component: UserList },
+  { path: "/pets", component: PetList },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+  linkActiveClass: "active",
+});
+
+//createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
