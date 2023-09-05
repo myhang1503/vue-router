c6a3d78d3f5a0e7b5c4b467d74bb52649573ae3d
ptmhang <myhang1503@gmail.com>
2023-09-05 21:05:16 +0700

    page note found

--- src/main.js
+++ src/main.js
@@ -1,21 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import UserList from "./pages/UserList.vue";
-import PetList from "./pages/PetList.vue";
-import { createRouter, createWebHistory } from "vue-router";
+
 import store from "./store";
+import router from "./routes";
 
-const routes = [
-  //{ path: "/", redirect: "/users" },
-  { path: "/users", component: UserList, alias: "/" },
-  { path: "/pets", component: PetList },
-];
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: routes,
-  linkActiveClass: "active",
-});
 
 //createApp(App).mount('#app')
 const app = createApp(App);
---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import UserList from "./pages/UserList.vue";
+import PetList from "./pages/PetList.vue";
+import UserDetail from "./pages/UserDetail.vue";
+import NotFound from "./pages/NotFound.vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  //{ path: "/", redirect: "/users" },
+  { path: "/users", component: UserList, alias: "/" },
+  { path: "/pets", component: PetList },
+  { path: "/user-detail", component: UserDetail },
+  { path: "/:pathMatch(.*)*", component: NotFound },
+  //{ path: "/:pathMatch(.*)*", redirect:"/" }, redirect to home page
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+  linkActiveClass: "active",
+});
+
+export default router;
